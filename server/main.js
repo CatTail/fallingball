6cfabe8c375214c5f0293ab596abfeebb3df4078
cattail <zhongchiyu@gmail.com>
2014-02-24 14:21:41 +0800

    cleanup: remove uncomplete server side code

--- server/main.js
+++
@@ -1,5 +0,0 @@
-exports.forms = require('./forms.js');
-exports.models = require('./models.js');
-exports.settings = require('./settings.js');
-exports.urls = require('./urls.js');
-exports.views = require('./views.js');
